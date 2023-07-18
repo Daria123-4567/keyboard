8e2d454d1a76cf69445698c9a3595a27f838a09a
Daria123-4567 <123991384+Daria123-4567@users.noreply.github.com>
2023-07-18 21:10:50 +0300

    add file

--- bd.py
+++ bd.py
@@ -1,30 +1,29 @@
 import psycopg2
+import sqlalchemy as sq
 from sqlalchemy.orm import Session
+from sqlalchemy.orm import declarative_base
+
 
 conn = psycopg2.connect(database="diplom", user="veronika", password="veronika")
+Base = declarative_base()
 
 
-def users():
-    with conn.cursor() as cursor:
-        cursor.execute(
-            """CREATE TABLE IF NOT EXISTS users(
-                    user_id SERIAL PRIMARY KEY,
-                    see_user_id SERIAL);"""
-        )
+class Users(Base):
+    __tablename__ = 'users'
+    user_id = sq.Column(sq.Integer, primary_key=True)
+    see_user_id = sq.Column(sq.Integer, primary_key=True)
 
+    def insert_into_users(self, user_id, see_user_id):
+        with Session(self) as session:
+            to_bd = Users(user_id=user_id, see_user_id=see_user_id)
+            session.add(to_bd)
+            session.commit()
 
-def insert_into_users(engine, user_id, see_user_id):
-    with Session(engine) as session:
-        to_bd = users(user_id=user_id, see_user_id=see_user_id)
-        session.add(to_bd)
-        session.commit()
-
-
-def check_users(engine, user_id, see_user_id):
-    with Session(engine) as session:
-        from_bd = session.query(users).filter(
-            users.user_id == user_id,
-            users.see_user_id == see_user_id
-        ).first()
-        return True if from_bd else False
+    def check_users(self, user_id, see_user_id):
+        with Session(self) as session:
+            from_bd = session.query(Users).filter(
+                Users.user_id == user_id,
+                Users.see_user_id == see_user_id
+            ).first()
+            return True if from_bd else False
 
