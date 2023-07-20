358ded0e4a87da2d51ccf6d2d75ff25a2689e756
Daria123-4567 <123991384+Daria123-4567@users.noreply.github.com>
2023-07-20 23:21:42 +0300

    add file

--- bd.py
+++ bd.py
@@ -8,16 +8,11 @@ conn = psycopg2.connect(database="diplom", user="veronika", password="veronika")
 Base = declarative_base()
 
 
-class Users(Base):
-    __tablename__ = 'users'
-    user_id = sq.Column(sq.Integer, primary_key=True)
-    see_user_id = sq.Column(sq.Integer, primary_key=True)
-
-    def insert_into_users(self, user_id, see_user_id):
-        with Session(self) as session:
-            to_bd = Users(user_id=user_id, see_user_id=see_user_id)
-            session.add(to_bd)
-            session.commit()
+def insert_into_users(self, user_id, see_user_id):
+    with Session(self) as session:
+        to_bd = Users(user_id=user_id, see_user_id=see_user_id)
+        session.add(to_bd)
+        session.commit()
 
     def check_users(self, user_id, see_user_id):
         with Session(self) as session:
@@ -26,4 +21,10 @@ class Users(Base):
                 Users.see_user_id == see_user_id
             ).first()
             return True if from_bd else False
+
+
+class Users(Base):
+    __tablename__ = 'users'
+    user_id = sq.Column(sq.Integer, primary_key=True)
+    see_user_id = sq.Column(sq.Integer, primary_key=True)
 
--- interface.py
+++ interface.py
@@ -2,7 +2,8 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 from config import token
-from core import VkWork
+from diplom import VkWork
+
 
 
 class VkBot:
@@ -10,6 +11,7 @@ class VkBot:
         self.bot = vk_api.VkApi(token=token)
         self.longpoll = VkLongPoll(self.vk)
         self.vk_work = VkWork(token)
+        self.profiles = {}
 
     def write_msg(self, user_id, message, photos=None):
         self.bot.method('messages.send', {'user_id': user_id,
@@ -23,9 +25,18 @@ class VkBot:
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                 self.write_msg(event.user_id, "Привет я бот для поиска...")
                 request = event.text
-
+                self.profiles = self.vk_work.users_get(event.user_id)
+                self.write_msg(event.user_id, f'здравствуй {self.profiles["name"]}')
                 if request == "ищи":
-                    self.write_msg(event.user_id, "я нашел")
+                    users = self.api.users_search(self.profiles)
+                    user = users.pop()
+                    photos = self.vk_work.photos_get(user['id'])
+                    attachment = ''
+                    for num, photo in enumerate(photos_user):
+                        attachment += f'photo{photo["owner_id"]}_{photo["id"]}'
+                        if num == 2:
+                            break
+                    self.write_msg(event.user_id, f'я нашел {user["name"]}', attachment=attachment)
                 elif request == "далее":
                     self.write_msg(event.user_id, "далее")
                 else:
